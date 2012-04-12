96430dfb05044491d054e74110f533d34b74a6bf
Squatingyeti <squatingyeti@yeticraft.net>
2012-04-12 16:28:21 -0500

    Import Essentials homes correctly & make sure player is in correct region before allowing sethome

--- src/net/yeticraft/squatingyeti/YetiHome/ImportData.java
+++ src/net/yeticraft/squatingyeti/YetiHome/ImportData.java
@@ -1,11 +1,7 @@
 package net.yeticraft.squatingyeti.YetiHome;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.util.List;
-
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -17,7 +13,6 @@ public class ImportData {
 		if (essentialsDir.exists()) {
 			Messaging.logInfo("Importing home locations from Essentials...", plugin);
 			File[] userFiles = essentialsDir.listFiles();
-			List<?> homeLocation; // <?> should be <Object>. But I couldn't compile that way. So, my javafu is weak.
 
 			for (File userFile : userFiles) {
 				try {
@@ -26,43 +21,19 @@ public class ImportData {
 						YamlConfiguration userConfig = new YamlConfiguration();
 						userConfig.load(userFile);
 
-						// Load old Essentials home format.
-						homeLocation = userConfig.getList("home");
-
-						if (homeLocation != null && !homeLocation.isEmpty()) {
-							try {
-								double X = (Double) homeLocation.get(0);
-								double Y = (Double) homeLocation.get(1);
-								double Z = (Double) homeLocation.get(2);
-								double pitch = (Double) homeLocation.get(4);
-								double yaw = (Double) homeLocation.get(3);
-								String world = (String) homeLocation.get(5);
-
-								out.write(user + ";" + X + ";" + Y + ";" + Z + ";" + pitch + ";" + yaw + ";" + world + ";" + Util.newLine());
-							} catch (Exception e) {
-									// This entry failed. Ignore and continue.
-									Messaging.logInfo("Failed to import home location!", plugin);
-									e.printStackTrace();
-							}
-						}
-
 						// Load new Essentials home format.
-						ConfigurationSection homeWorlds = userConfig.getConfigurationSection("home.worlds");
+						ConfigurationSection homeWorlds = userConfig.getConfigurationSection("homes.home");
 						//Map<String, ConfigurationNode> homeWorlds = userConfig.getNodes("home.worlds");
 
 						if (homeWorlds != null) {
-							for (String homeWorld : homeWorlds.getKeys(false)) {
-								ConfigurationSection homeData = userConfig.getConfigurationSection("home.worlds." + homeWorld);
-
-								if (homeData != null) {
 									try {
-										double X = homeData.getDouble("x", 0);
-										double Y = homeData.getDouble("y", 0);
-										double Z = homeData.getDouble("z", 0);
-										double pitch = homeData.getDouble("pitch", 0);
-										double yaw = homeData.getDouble("yaw", 0);
-										String world = homeData.getString("world");
-
+										double X = homeWorlds.getDouble("x", 0);
+										double Y = homeWorlds.getDouble("y", 0);
+										double Z = homeWorlds.getDouble("z", 0);
+										double pitch = homeWorlds.getDouble("pitch", 0);
+										double yaw = homeWorlds.getDouble("yaw", 0);
+										String world = homeWorlds.getString("world");
+										
 										out.write(user + ";" + X + ";" + Y + ";" + Z + ";" + pitch + ";" + yaw + ";" + world + ";" + Util.newLine());
 									} catch (Exception e) {
 											// This entry failed. Ignore and continue.
@@ -70,73 +41,9 @@ public class ImportData {
 											e.printStackTrace();
 									}
 								}
-							}
-						}
 				} catch (Exception e) {
 						Messaging.logWarning("Error importing from " + userFile.getName() + ": " + e.getMessage(), plugin);
 				}
-			}
-		}
-	}
-
-	public static void importHomesFromMultipleHomes(BufferedWriter out, YetiHome plugin) {
-		File essentialsDir = new File("plugins" + File.separator + "MultipleHomes" + File.separator + "Homes");
-
-		if (essentialsDir.exists()) {
-			Messaging.logInfo("Importing home locations from MultipleHomes...", plugin);
-			File[] userFiles = essentialsDir.listFiles();
-
-			for (File userFile : userFiles) {
-					String name = userFile.getName().replaceAll("home\\_", "").replaceAll("\\.txt", "");
-
-					try {
-						FileReader fstream = new FileReader(userFile);
-						BufferedReader reader = new BufferedReader(fstream);
-
-						String line = reader.readLine().trim();
-
-						while (line != null) {
-							if (line.startsWith("~")) {
-								String[] split = line.split(":");
-
-								if (split.length == 2) {
-									String user = split[0].substring(1);
-									String[] values = split[1].split("_");
-									double X = 0, Y = 0, Z = 0;
-									float pitch = 0, yaw = 0;
-									String world = "";
-									int homeNumber;
-
-									try {
-										X = Double.parseDouble(values[0]);
-										Y = Double.parseDouble(values[1]);
-										Z = Double.parseDouble(values[2]);
-										pitch = Float.parseFloat(values[4]);
-										yaw = Float.parseFloat(values[3]);
-										world = values[5];
-										homeNumber = Integer.parseInt(name);
-
-										if (homeNumber == 0) {
-											out.write(user + ";" + X + ";" + Y + ";" + Z + ";" + pitch + ";" + yaw + ";" + world + ";" + Util.newLine());
-										} else {
-											out.write(user + ";" + X + ";" + Y + ";" + Z + ";" + pitch + ";" + yaw + ";" + world + ";" + homeNumber + Util.newLine());
-										}
-									} catch (Exception e) {
-										// This entry failed. Ignore and continue.
-										if (line!=null) {
-											Messaging.logWarning("Failed to load home location! Line: " + line, plugin);
-										}
-									}
-								}
-							}
-
-							line = reader.readLine();
-						}
-
-						reader.close();
-					} catch (Exception e) {
-						Messaging.logSevere("Failed to import homes from MultipleHomes!", plugin);
-					}
 			}
 		}
 	}
--- src/net/yeticraft/squatingyeti/YetiHome/YetiHome.java
+++ src/net/yeticraft/squatingyeti/YetiHome/YetiHome.java
@@ -1,12 +1,26 @@
 package net.yeticraft.squatingyeti.YetiHome;
 
+import static com.sk89q.worldguard.bukkit.BukkitUtil.toVector;
+
 import java.io.File;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldedit.Vector;
 
 public class YetiHome extends JavaPlugin {
 	public HomeManager homes;
@@ -93,7 +107,10 @@ public class YetiHome extends JavaPlugin {
 					Settings.sendMessageTooManyParameters(player);
 			}
 		} else if (cmd.getName().equalsIgnoreCase("sethome")) {
-			
+			if (!compliantHome(player.getLocation(), player.getName())) {
+				player.sendMessage(ChatColor.RED + "This area does not smell friendly");
+				return;
+			}
 			if (args.length == 0) {
 				this.commandExecutor.setDefaultHome(player);
 			
@@ -105,7 +122,7 @@ public class YetiHome extends JavaPlugin {
 				} else {
 						this.commandExecutor.setNamedHome(player, homeArgs[0]);
 				}
-					
+				
 			} else {
 					Settings.sendMessageTooManyParameters(player);
 			}
@@ -126,4 +143,40 @@ public class YetiHome extends JavaPlugin {
 				}
 			}
 		}
+	
+	public WorldGuardPlugin getWorldGuard() {
+	    Plugin plugin = getServer().getPluginManager().getPlugin("WorldGuard");
+	 
+	    // WorldGuard may not be loaded
+	    if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+	        return null; // Maybe you want throw an exception instead
+	    }
+	 
+	    return (WorldGuardPlugin) plugin;
 	}
+	
+	public boolean compliantHome(Location loc, String player) {
+		
+		WorldGuardPlugin worldGuard = getWorldGuard();
+		
+		Vector pt = toVector(loc);
+		
+		RegionManager regionManager = worldGuard.getRegionManager(loc.getWorld());
+		List<String> regionSet = regionManager.getApplicableRegionsIDs(pt);
+		
+		PermissionManager pexPlayer = PermissionsEx.getPermissionManager();
+		PermissionUser pPlayer = pexPlayer.getUser(player);
+		
+		boolean inRegionGroup = false;
+		
+		for(String currentRegion : regionSet){
+	
+			if (pPlayer.inGroup(currentRegion.toLowerCase())) inRegionGroup = true;
+			
+		}
+		
+	if (inRegionGroup) return true;
+	return false;
+	
+	}
+}
